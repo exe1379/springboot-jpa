9b228227ad7ea7a5bd58e0b25bfa83d11d23ce97
Zenboo 13 <Zenboo 13@DESKTOP-N0S4I95>
2025-05-16 16:54:32 +0800

    jpa-roomtest

--- src/main/java/com/example/demo/repository/RoomRepository.java
+++ src/main/java/com/example/demo/repository/RoomRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.entity.Room;
 
-public interface RoomRepository extends JpaRepository<Room, Integer>{
+public interface RoomRepository extends JpaRepository<Room, Integer>{ // Room: entity, Integer : @Id 的型別
 	
 }
