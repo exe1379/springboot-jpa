feaf4af50b09f3e100eba720c6884d2b4d0e1cdb
Zenboo 13 <Zenboo 13@DESKTOP-N0S4I95>
2025-05-16 16:36:28 +0800

    jpa

--- src/main/java/com/example/demo/model/entity/Room.java
+++ src/main/java/com/example/demo/model/entity/Room.java
@@ -1,10 +1,11 @@
 package com.example.demo.model.entity;
 
-import org.springframework.data.annotation.Id;
+
 import org.springframework.data.relational.core.mapping.Table;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,8 @@ import lombok.NoArgsConstructor;
 @Entity //代表這個類別是一個 Java 的實體類別，會與資料表進行對應(會自動建立資料表)
 @Table(name = "room") // 若資料表名稱與類別名稱一致可省略此行
 public class Room {
-	@Id
+	@Id //代表這是一個主鍵
+	// @GeneratedValue(strategy = GeneratedType.IDENTITY) 讓room_id從一開始生成,每次加1
 	@Column(name = "room_id")
 	private Integer roomId;
 	@Column(name="room_name", nullable = false , unique = true)
