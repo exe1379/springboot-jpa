0194e1bef4cc347c63b93d9c89cc4d223b6196cb
Zenboo 13 <Zenboo 13@DESKTOP-N0S4I95>
2025-05-19 11:26:09 +0800

    serviceImpl

---
+++ src/main/java/com/example/demo/Exception/RoomAlreadyExistException.java
@@ -0,0 +1,8 @@
+package com.example.demo.Exception;
+
+public class RoomAlreadyExistException extends RoomException{
+
+	public RoomAlreadyExistException(String message) {
+		super(message);
+	}
+}
--- src/main/java/com/example/demo/service/Impl/RoomServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/RoomServiceImpl.java
@@ -1,35 +1,50 @@
 package com.example.demo.service.Impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.Exception.RoomAlreadyExistException;
+import com.example.demo.Exception.RoomNotFoundException;
+import com.example.demo.method.RoomMapper;
 import com.example.demo.model.dto.RoomDto;
+import com.example.demo.model.entity.Room;
 import com.example.demo.repository.RoomRepository;
 import com.example.demo.service.RoomService;
 @Service
 public class RoomServiceImpl implements RoomService {
 	@Autowired
 	private RoomRepository roomRepository;
-	
+	@Autowired
+	private RoomMapper roomMapper;
 	
 	@Override
 	public List<RoomDto> findAllRooms() {
-		// TODO Auto-generated method stub
-		return null;
+		return roomRepository.findAll()
+							 .stream()
+							 .map(roomMapper::toDto)
+							 .toList();
 	}
 
 	@Override
 	public RoomDto getRoomById(Integer roomId) {
-		// TODO Auto-generated method stub
-		return null;
+		Room room = roomRepository.findById(roomId).orElseThrow(() -> new RoomNotFoundException("找不到該房間"));
+		return roomMapper.toDto(room);
 	}
 
 	@Override
 	public void addRoom(RoomDto roomDto) {
-		// TODO Auto-generated method stub
-
+		// 先判斷該房號是否已存在
+		Optional<Room> optRoom = roomRepository.findById(roomDto.getRoomId());
+		// 若存在則拋出例外
+		if(optRoom.isPresent()) {
+			throw new RoomAlreadyExistException("新增失敗: 房號 " + roomDto.getRoomId() + " 已經存在");
+		}
+		// 不存在則建立一個新的 Room 物件(RoomDto 轉換而成)
+		Room room = roomMapper.toEntity(roomDto);
+		roomRepository.save(room);
 	}
 
 	@Override
